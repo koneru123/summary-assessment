611bbdf4e68713601247db7770946cc4f6aa2ef2
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-09 09:39:28 -0700

    basic-closure implementation

--- basic-closure/basic-closure.js
+++ basic-closure/basic-closure.js
@@ -14,6 +14,35 @@ startAtSixteen() // -> returns 19
 
 */
 
+/* //makePlusOne function has one parameter, x , which is an integer
 var makePlusOne = function(x) {
-  // YOUR CODE HERE
+  // Create a variable incrementNumByOne and initilize it to x
+  var incrementNumByOne = x;
+  // makePlusOne function is expected to return a function, so returning an anonymous function
+  return function() {
+    // this is a closure function, since it can access the outer function variables
+    // here we are incrementing the incrementNumByOne by 1 every time the function is invoked
+    incrementNumByOne += 1;
+    //return the variable incrementNumByOne
+    return incrementNumByOne;
+  }
+}; */
+
+// same as above except that I am trying to use an arrow function
+var makePlusOne = (x) => {
+  // Create a variable incrementNumByOne and initilize it to x
+  var incrementNumByOne = x;
+  // makePlusOne function is expected to return a function, so returning an anonymous function
+  return () => {
+    // this is a closure function, since it can access the outer function variables
+    // here we are incrementing the incrementNumByOne by 1 every time the function is invoked
+    incrementNumByOne += 1;
+    //return the variable incrementNumByOne
+    return incrementNumByOne;
+  }
 };
+
+var startAtSixteen = makePlusOne(16);
+console.log(startAtSixteen()); // returns 17
+console.log(startAtSixteen()); // returns 18
+console.log(startAtSixteen()); // returns 19
