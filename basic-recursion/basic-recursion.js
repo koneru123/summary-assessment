4b97157d40e4b4d72558d7f5bfd8a4bc1d49dd9f
Priyanka Koneru <koneru.priyanka@gmail.com>
2020-03-09 09:56:10 -0700

    basic-recursion implementation

--- basic-recursion/basic-recursion.js
+++ basic-recursion/basic-recursion.js
@@ -27,5 +27,37 @@
 */
 
 var recursionNumSum = function (object) {
-  // YOUR CODE HERE
+  // Create a variable endResult and assign it to 0 (since expected result is an integer value)
+  var endResult = 0;
+  // loop through the object for values
+  for(let value of Object.values(object)) {
+    // check if the datatype of each value is integer
+    if(typeof(value) === 'number') {
+      // if it is an integer keep adding to the endResult variable
+      endResult += value
+      // if in case it has nested object as value, check if the datatype of each value is an object
+    } else if(typeof(value) === 'object') {
+      // call the function(recursionNumSum) recursively with value as a function argument
+      // add the result of the recursive function to the endResult value
+      endResult += recursionNumSum(value);
+    }
+  }
+  // return the variable endResult
+  return endResult;
 };
+
+var example = {
+  name: 'Alfred',
+  age: 42,
+  house: {
+    inhabitants: 2,
+    address: {
+      number: 11,
+      city: 'Rochester',
+      street: 'Fourth',
+      zip: 13555
+    }
+  }
+}
+
+console.log(recursionNumSum(example)); // return 13610
